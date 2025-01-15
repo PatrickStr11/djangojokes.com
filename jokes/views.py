6ecc00260246579d70b0040eabe9a2165dca61df
PatrickStr11 <p.jamesdguzman@gmail.com>
2025-01-16 07:17:46 +0800

    9th Commit

--- jokes/views.py
+++ jokes/views.py
@@ -1,10 +1,15 @@
-from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.views.generic import (CreateView, DeleteView, DetailView, ListView, UpdateView)
 from .models import Joke
+from django.urls import reverse_lazy
 
 # Create your views here.
 class JokeCreateView(CreateView):
     model = Joke
     fields = ['question', 'answer']
+    
+class JokeDeleteView(DeleteView):
+    model = Joke
+    success_url = reverse_lazy('jokes:list')
 
 class JokeDetailView(DetailView):
     model = Joke
