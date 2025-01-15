917d3079a32f758a84e5ffdd316fb471dfc9575e
PatrickStr11 <p.jamesdguzman@gmail.com>
2025-01-15 12:23:49 +0800

    6th Commit

--- jokes/views.py
+++ jokes/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Joke
+
+class JokeListView(ListView):
+    model = Joke
 
 # Create your views here.
